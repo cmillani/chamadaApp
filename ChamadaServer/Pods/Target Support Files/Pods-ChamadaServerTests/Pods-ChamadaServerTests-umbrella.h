f3408d10107eff9928f20a65e8abcd3a50227213
cmillani <carloseduardomillani@gmail.com>
2016-11-26 14:39:37 -0200

    Project created and pod added

---
+++ ChamadaServer/Pods/Target Support Files/Pods-ChamadaServerTests/Pods-ChamadaServerTests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_ChamadaServerTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_ChamadaServerTestsVersionString[];
+
